54ad51b59c7b07458753f9ee79107ebb6448f884
CsrCarvalho19 <cesar.carvalho19@live.com>
2020-11-13 21:01:58 -0300

    att files

---
+++ frontend/src/config/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        isMenuVisible: true,
+        user: {
+            name: 'César Carvalho',
+            email: 'cesar.silva57@fatec.sp.gov.br'
+        }
+    },
+    mutations: {
+        toggleMenu(state, isVisible) {
+            if(isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+        }
+    }
+})
